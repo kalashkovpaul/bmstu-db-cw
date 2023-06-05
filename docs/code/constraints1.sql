ea7c59d80a157f90980969abdb2d4782f79adcbd
Pavel Kalashkov <p.kalashkov@vk.team>
2023-06-05 10:57:48 +0300

    Ready to show

--- docs/code/constraints1.sql
+++ docs/code/constraints1.sql
@@ -2,7 +2,7 @@ ALTER TABLE patients ADD CONSTRAINT addr_constraint
     FOREIGN KEY (address_id) REFERENCES addresses(address_id)
     ON DELETE CASCADE;
 
-ALTER TABLE accesses ADD CONSTRAINT agreement_patient_constraint
+ALTER TABLE accesses ADD CONSTRAINT agr_patient_constraint
     FOREIGN KEY (staff_id) REFERENCES staff(staff_id)
     ON DELETE CASCADE;
 
